f89c7a725bab1011b8d79aedf03744980515ddbc
V4Vince <vince.guoo@gmail.com>
2016-04-05 14:02:10 -0400

    adds hover
    indicated the current selection with hover

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,3 +1,19 @@
 'use strict';
 
+
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+    let gameboard = [0, 1, 2, 3, 4, 5, 6, 7 ,8];
+
+    $('.gameboard').find('div').hover(function(){
+      $(this).addClass("showSelection");
+    }, function(){$(this).removeClass("showSelection");
+    }
+  );
+});
+
+
+
+
 module.exports = true;
