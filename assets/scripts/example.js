c654562e843b5fb35aad18d4d292b56f9cf92a8a
V4Vince <vince.guoo@gmail.com>
2016-04-05 15:28:26 -0400

    adds player class
    adds player class and win conditions

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -1,16 +1,26 @@
 'use strict';
-
+/*
+let winCondition = [0, 1, 2], [3, 4, 5], [6, 7, 8],//rows
+                   [0, 3, 6], [1, 4, 5], [2, 5, 8],//columns
+                   [1, 5, 9], [3, 5, 7];//across
+*/
 
 $( document ).ready(function() {
-    console.log( "ready!" );
+    console.log( 'ready!' );
 
     let gameboard = [0, 1, 2, 3, 4, 5, 6, 7 ,8];
+    let currentPlayer = $('playerX');
 
-    $('.gameboard').find('div').hover(function(){
-      $(this).addClass("showSelection");
-    }, function(){$(this).removeClass("showSelection");
-    }
-  );
+    $('.gameboard').find('.squares').hover(function(){
+      $(this).addClass('showSelection');
+    }, function(){
+      $(this).removeClass('showSelection');
+  });
+
+    $('.gameboard').find('.squares').click(function(){
+    $(this).addClass('playerX');
+  });
+
 
 });
 
