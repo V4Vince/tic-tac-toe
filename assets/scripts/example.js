0f61268a03f585f6e9450c9cb0f9c936501434e4
V4Vince <vince.guoo@gmail.com>
2016-04-06 13:26:22 -0400

    completed all win conditions!!

--- assets/scripts/example.js
+++ assets/scripts/example.js
@@ -8,35 +8,67 @@ let winCondition = [0, 1, 2], [3, 4, 5], [6, 7, 8],//rows
 let gameBoard = [$("#one"), $("#two"), $("#three"),
                  $("#four"), $("#five"), $("#six"),
                   $("#seven"), $("#eight"), $("#nine")];
+*/
+
 
 let topRow = [$("#one"), $("#two"), $("#three")];
-let middleRow = [$("#four"), $("#five"), $("##six")];
+let middleRow = [$("#four"), $("#five"), $("#six")];
 let bottomRow = [$("#seven"), $("#eight"), $("#nine")];
 let firstCol = [$("#one"), $("#four"), $("#seven")];
 let secondCol = [$("#two"), $("#five"), $("#eight")];
 let thirdCol = [$("#three"), $("#six"), $("#nine")];
-*/
 
-let topRow = [$("#one"), $("#two"), $("#three")];
+
 
 let currentSquare;
 let turns = 0;
 let currentPlayer = null;
 let playerX = 'X';
 let playerO = 'O';
-let gameOver;
+//let gameOver;
 let playerXCounter = 0;
 
 
-const checkWinner = function(){
-  for (var i = 0; i < topRow.length; i++) {
-    if (topRow[i].hasClass("playerX")) {
+const checkConditions = function(winCondition){
+  for (var i = 0; i < winCondition.length; i++) {
+    if (winCondition[i].hasClass("playerX")) {
       console.log(playerXCounter++);
-    } else if(topRow[i].hasClass("playerO")){
+    } else if(winCondition[i].hasClass("playerO")){
       console.log(playerXCounter--);
     }
   }//for
 };//checkWinner
+
+const displayWinner = function(){
+  if (turns === 9) {
+    console.log("Its a draw!");
+  } else if (playerXCounter === 3) {
+    console.log("Player X Wins");
+    playerXCounter = 0;
+  } else if (playerXCounter === -3) {
+    console.log("Player O Wins");
+    playerXCounter = 0;
+  } else {
+    playerXCounter = 0;
+  }
+};
+
+const getWinner = function(){
+  if (turns >= 4) {
+    checkConditions(topRow);
+    displayWinner();
+    checkConditions(middleRow);
+    displayWinner();
+    checkConditions(bottomRow);
+    displayWinner();
+    checkConditions(firstCol);
+    displayWinner();
+    checkConditions(secondCol);
+    displayWinner();
+    checkConditions(thirdCol);
+    displayWinner();
+  }
+};
 
 
 //Listens for clicks on squares
@@ -57,66 +89,10 @@ $('.gameboard').find('.squares').click(function(){
     }//else
   }//if
 
-  if (turns >= 4) {
-    checkWinner();
-  }
 
-  if (turns === 9) {
-    console.log("Its a draw!");
-  } else if (playerXCounter === 3) {
-    console.log("Player X Wins");
-    playerXCounter = 0;
-  } else if (playerXCounter === -3) {
-    console.log("Player O Wins");
-    playerXCounter = 0;
-  } else {
-    playerXCounter = 0;
-  }
-
-
-
-
-
-
-/*
-
-  if (turns >= 4) {
-    for (var i = 0; i < topRow.length; i++) {
-      if (!topRow[i].hasClass("playerX")) {
-        gameOver = false;
-      } else if (!middleRow[i].hasClass("playerX")) {
-
-      }{
-
-      }//else
-    }//for
-
-  }//if
-
-
-
-*/
+  getWinner();
 
 
 });
 
-/*
-  if (turns >= 4) {
-    $('top-row').children('squares').each(function(){
-      if ($(this).hasClass('playerX')) {
-        playerXCounter++;
-      }
-    });
-  }
-*/
-
-/*
-
-$('.gameboard').find('.squares').hover(function(){
-  $(this).text(currentPlayer).addClass('player');
-}, function(){
-  $(this).text("").removeClass('player');
-});
-
-*/
 module.exports = true;
